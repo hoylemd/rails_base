f7200ee466c457016a4798593742d607b7fc8bf2
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-02 10:48:41 -0500

    Add tests for full_title

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,7 +11,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', contact_path
 
     get signup_path
-    assert_select 'title' 'Sign Up | Ruby on Rails Tutorial Sample App'
+    assert_select 'title', full_title('Sign Up')
 
   end
 end
