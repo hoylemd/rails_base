7fdd8aa00d9b47fd99437b4f86c5573914d2d5e3
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-02 10:29:29 -0500

    add some more to the integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
 
+    get signup_path
+    assert_select 'title' 'Sign Up | Ruby on Rails Tutorial Sample App'
+
   end
 end
