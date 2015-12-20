2c3eead9c0291e76d7d5e242322dd30ec8cd7666
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-20 17:10:43 -0500

    add valid email tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,25 +11,35 @@ class UserTest < ActiveSupport::TestCase
 
   test "should have name" do
     @batman.name = ""
-    assert_not @batman.valid?
+    assert_not @batman.valid?, "empty string should be invalid"
     @batman.name = "      "
-    assert_not @batman.valid?
+    assert_not @batman.valid?, "whitespace only should be invalid"
   end
 
   test "name should not be too long" do
     @batman.name = 'a' * 51
-    assert_not @batman.valid?
+    assert_not @batman.valid?, "#{@batman.name.length} should be too long"
   end
 
   test "should have email" do
     @batman.email = ""
-    assert_not @batman.valid?
+    assert_not @batman.valid?, "empty string should be invalid"
     @batman.email = "      "
-    assert_not @batman.valid?
+    assert_not @batman.valid?, "whitespace only should be invalid"
   end
 
   test "email should not be too long" do
     @batman.email = 'a' * 244 + '@example.com'
-    assert_not @batman.valid?
+    assert_not @batman.valid?, "#{@batman.email.length} should be too long"
+  end
+
+  test "valid emails should be accepted" do
+    emails = ["hello@example.com", "hello+tagged@example.com",
+              "hi.myname@example.com", "lOo0ks_w13rd@example.com", "n@jp"]
+
+    emails.each do |email|
+      @batman.email = email
+      assert @batman.valid?, "#{email} should be valid"
+    end
   end
 end
