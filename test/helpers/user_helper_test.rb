6d70e47649a6ca764b8d8cf4230c8c7610274e0e
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-22 17:31:51 -0500

    Add a test for the gravatar helper

---
+++ test/helpers/user_helper_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersHelperTest < ActionView::TestCase
+  test 'gravatar_for generates corect html' do
+    user = User.new(email: 'peachesthefriendlyorc@gmail.com',
+                    name: 'Peaches the Friendly Orc')
+
+    assert_equal(gravatar_for(user),
+                 "<img alt=\"Peaches the Friendly Orc\" class=\"gravatar\" " \
+                 "src=\"https://secure.gravatar.com/avatar/" \
+                 "0298badedee3c87e93655f53ebab9fe0\" />")
+  end
+end
