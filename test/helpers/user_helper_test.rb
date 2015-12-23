cd785333c90b7a463d0cc0155f64f84b76b17b21
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-23 14:43:59 -0500

    Add size option to gravatar helper

--- test/helpers/user_helper_test.rb
+++ test/helpers/user_helper_test.rb
@@ -6,8 +6,8 @@ class UsersHelperTest < ActionView::TestCase
                     name: 'Peaches the Friendly Orc')
 
     assert_equal(gravatar_for(user),
-                 "<img alt=\"Peaches the Friendly Orc\" class=\"gravatar\" " \
-                 "src=\"https://secure.gravatar.com/avatar/" \
-                 "0298badedee3c87e93655f53ebab9fe0\" />")
+                 '<img alt="Peaches the Friendly Orc" class="gravatar" ' \
+                 'src="https://secure.gravatar.com/avatar/' \
+                 '0298badedee3c87e93655f53ebab9fe0?size=80" />')
   end
 end
