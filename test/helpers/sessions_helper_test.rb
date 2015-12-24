0ff297788779aa14f26d4ade101eb75e1eafe19a
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-24 16:46:39 -0500

    Add test for current_user

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
+  def setup
+    @kylo = User.create(name: 'Kylo Ren', email: 'vaderFan667@hotmail.com',
+                        password: 'starkiller',
+                        password_confirmation: 'starkiller')
+  end
+
   test 'log_in stores the passed user_id in the session' do
     assert session[:user_id].nil?, 'session should not have a user id'
-    log_in(User.create(name: 'Kylo Ren', email: 'vaderFan667@hotmail.com',
-                       password: 'starkiller',
-                       password_confirmation: 'starkiller'))
+    log_in(@kylo)
     assert session[:user_id], 'session should have a user id'
   end
 
   test 'current_user returns the current user or nil' do
+    assert @current_user.nil?, '@current_user should be nil'
+    session[:user_id] = @kylo.id
+    assert current_user == @kylo, 'current_user should return Kylo Ren'
+    assert @current_user == @kylo, '@current_user should be Kylo Ren'
   end
 end
