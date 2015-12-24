219c662cb268378001a388747795a17bb4f00394
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-24 16:51:15 -0500

    Add logged+in helper

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -9,14 +9,23 @@ class SessionsHelperTest < ActionView::TestCase
 
   test 'log_in stores the passed user_id in the session' do
     assert session[:user_id].nil?, 'session should not have a user id'
+
     log_in(@kylo)
     assert session[:user_id], 'session should have a user id'
   end
 
   test 'current_user returns the current user or nil' do
     assert @current_user.nil?, '@current_user should be nil'
+
     session[:user_id] = @kylo.id
     assert current_user == @kylo, 'current_user should return Kylo Ren'
     assert @current_user == @kylo, '@current_user should be Kylo Ren'
+  end
+
+  test 'logged_in? tells if someone is logged in' do
+    assert_not logged_in?, 'should not be logged in'
+
+    session[:user_id] = @kylo.id
+    assert logged_in?, 'should be logged in'
   end
 end
