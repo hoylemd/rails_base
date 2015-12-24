6e84fd0a6434ab583df88b9eeef401f085c9c485
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-24 16:28:54 -0500

    log in and redirect user on login and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome, #{@user.name}!"
+      log_in @user
       redirect_to @user
     else
       render 'new'
