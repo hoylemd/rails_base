170cad6d82caf67ec12429b144accbb5d23e1285
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 15:44:12 -0500

    fix assert_equal argument order
    I was doing <actual>, <expected>, but Best Practices is <expected>,
    <actual>

--- test/helpers/user_helper_test.rb
+++ test/helpers/user_helper_test.rb
@@ -4,18 +4,18 @@ class UsersHelperTest < ActionView::TestCase
   test 'gravatar_for generates corect html' do
     user = users(:peaches)
 
-    assert_equal(gravatar_for(user),
-                 '<img alt="Peaches the Friendly Orc" class="gravatar" ' \
+    assert_equal('<img alt="Peaches the Friendly Orc" class="gravatar" ' \
                  'src="https://secure.gravatar.com/avatar/' \
-                 '0298badedee3c87e93655f53ebab9fe0?size=80" />')
+                 '0298badedee3c87e93655f53ebab9fe0?size=80" />',
+                 gravatar_for(user))
   end
 
   test 'gravatar_for accepts size parameter' do
     user = users(:peaches)
 
-    assert_equal(gravatar_for(user, size: 234),
-                 '<img alt="Peaches the Friendly Orc" class="gravatar" ' \
+    assert_equal('<img alt="Peaches the Friendly Orc" class="gravatar" ' \
                  'src="https://secure.gravatar.com/avatar/' \
-                 '0298badedee3c87e93655f53ebab9fe0?size=234" />')
+                 '0298badedee3c87e93655f53ebab9fe0?size=234" />',
+                 gravatar_for(user, size: 234))
   end
 end
