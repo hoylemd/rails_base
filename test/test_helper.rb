f194a98844e2d855083e0767d2ee7219b86959ef
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 15:12:08 -0500

    Add some login test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,5 +15,25 @@ module ActiveSupport
     def logged_in?
       !session[:user_id].nil?
     end
+
+    # Logs in a test user.
+    def log_in_as(user, options = {})
+      if integration_test?
+        password    = options[:password] || 'password'
+        remember_me = options[:remember_me] || '1'
+        post login_path, session: { email:       user.email,
+                                    password:    password,
+                                    remember_me: remember_me }
+      else
+        session[:user_id] = user.id
+      end
+    end
+
+    private
+
+    # Returns true inside an integration test.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
   end
 end
