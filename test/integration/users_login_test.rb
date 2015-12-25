5a8854a85975e749eaa437950d712571b092a67b
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 15:33:15 -0500

    Add tests for remembering

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -37,5 +37,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', user_path(@kylo), count: 0
   end
 
-  # TODO: 'login with valid info, and remembering'
+  test 'login with remembering' do
+    log_in_as(@kylo, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test 'login without remembering' do
+    log_in_as(@kylo, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
 end
