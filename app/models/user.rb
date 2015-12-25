f9552f936320bd7a78e4707f4e18f4d13b4f26cf
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-24 21:13:42 -0500

    Implement remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)*([a-z]+)\z/i
+  attr_accessor :remember_token
 
   before_save do
     email.downcase!
@@ -26,5 +27,10 @@ class User < ActiveRecord::Base
   # Returns a random token.
   def self.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
