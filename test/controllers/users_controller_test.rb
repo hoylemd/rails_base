ab3e8282836c7c052a41fa5b68144517d7165833
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-26 14:08:05 -0500

    Move the signup tests to integration tests
    They really belonged there, since they weren't testing the controller in
    isolation
    
    I also added a helper function to make it easier to assert that a signup
    was successful.
    
    I'm not done refactoring these to be proper integration tests though.
    More commits to come.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,81 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  def setup
-    @test_info = { name: 'Max Payne',
-                   email: 'Max_Payne@example.com',
-                   password: 'password',
-                   password_confirmation: 'password' }
-    @kylo = users(:kylo)
-  end
-
   test 'should get new' do
     get :new
     assert_response :success
     assert_select 'title', 'Sign Up | Ruby on Rails Tutorial Sample App'
-  end
-
-  # TODO: These should be moved to integration tests
-  test 'should redirect on post to create with valid information' do
-    post :create, user: { name: 'George Carlin',
-                          email: 'george_carlin@example.com',
-                          password: 'password',
-                          password_confirmation: 'password' }
-    assert_response :redirect, 'should redirect to user profile'
-
-    post :create, user: { name: 'Luke Skywlker',
-                          email: 'the_last_jedi@example.com',
-                          password: 'password',
-                          password_confirmation: 'password',
-                          id: 'so lonely :(' }
-    assert_response :redirect, 'should ignore extra parameters'
-    # This tests user_params - the id field will be stripped out, and not cause
-    # an error.  if it were not stripped out, an error would be raised, as ids
-    # are integers, not strings
-  end
-
-  test 'should error on post with missing name' do
-    post :create, user: @test_info.merge(name: '')
-    assert_response 422, 'should error on missing name'
-    assert_select '.field_with_errors input#user_name', 1,
-                  'name field should be highlighted'
-  end
-
-  test 'should error on post with invalid email' do
-    post :create, user: @test_info.merge(email: '')
-    assert_response 422, 'should error on missing email'
-    assert_select '.field_with_errors input#user_email', 1,
-                  'email field should be highlighted'
-
-    post :create, user: @test_info.merge(email: 'i am not an email address')
-    assert_response 422, 'should error on invalid email'
-    assert_select '.field_with_errors input#user_email', 1,
-                  'email field should be highlighted'
-
-    post :create, user: @test_info.merge(email: @kylo.email)
-    assert_response 422, 'should error on already-taken password'
-    assert_select '.field_with_errors input#user_email', 1,
-                  'email field should be highlighted'
-  end
-
-  test 'should error on post with invalid password or confirmation' do
-    post :create, user: @test_info.merge(password: '',
-                                         password_confirmation: '')
-    assert_response 422, 'should error on missing password and confirmation'
-    assert_select '.field_with_errors input#user_password', 1,
-                  'should highlight password field'
-
-    post :create, user: @test_info.merge(password: 'short',
-                                         password_confirmation: 'short')
-    assert_response 422, 'should error on too-short password'
-    assert_select '.field_with_errors input#user_password', 1,
-                  'should highlight password field'
-
-    post :create, user: @test_info.merge(password: 'longbutwrong',
-                                         password_confirmation: 'wrongandlong')
-    assert_response 422, 'should error on mismatched password and confirmation'
-    assert_select '.field_with_errors input#user_password_confirmation', 1,
-                  'should highlight password confirmation field'
   end
 
   # I can't figure out how to go the GET here. Keeps saying:
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,34 +1,98 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test 'invalid signup information is rejected' do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: { name:  '',
-                               email: 'user@invalid',
-                               password:              'foo',
-                               password_confirmation: 'bar' }
-    end
-    assert_template 'users/new'
+  def setup
+    @test_info = { name: 'Max Payne',
+                   email: 'Max_Payne@example.com',
+                   password: 'password',
+                   password_confirmation: 'password' }
+    @kylo = users(:kylo)
+  end
 
-    assert_select 'div#error_explanation'
-    assert_select 'div.field_with_errors'
+  def assert_signup_successful(name = nil)
+    assert_template 'users/show', 'should be on profile page'
+    assert_select '.alert-danger', false, 'flash should not contain errors'
+    flash_expected = name ? "Welcome, #{name}!" : 1
+    assert_select '.alert-success', flash_expected
+    assert logged_in?, 'User should be logged in'
   end
 
   test 'valid signup information is accepted' do
+    name = 'Max Kanata'
+
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name:  'Example User',
-                                            email: 'user@example.com',
-                                            password:              'password',
+      post_via_redirect users_path, user: { name: name,
+                                            email: 'maz@example.com',
+                                            password: 'password',
                                             password_confirmation: 'password',
                                             remember_me: '1' }
     end
-    assert_template 'users/show'
 
-    assert_select 'div#error_explanation', false
-    assert_select 'div.field_with_errors', false
+    assert_signup_successful name
+  end
 
-    assert logged_in?, 'User should be logged in'
+  test 'post to create should ignore extra parameters' do
+    name = 'Luke Skywalker'
+
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: name,
+                                            email: 'the_last_jedi@example.com',
+                                            password: 'password',
+                                            password_confirmation: 'password',
+                                            id: 'so lonely :(' }
+    end
+
+    assert_signup_successful name
+    # This tests user_params - the id field will be stripped out, and not cause
+    # an error.  If it were not stripped out, an error would be raised because
+    # ids are integers - not strings.
+  end
+
+  test 'should error on post with missing name' do
+    assert_no_difference 'User.count' do
+      post users_path, user: @test_info.merge(name: '')
+    end
+    assert_response 422, 'should error on missing name'
+    assert_select '.field_with_errors input#user_name', 1,
+                  'name field should be highlighted'
+  end
+
+  test 'should error on post with invalid email' do
+    post users_path, user: @test_info.merge(email: '')
+    assert_response 422, 'should error on missing email'
+    assert_select '.field_with_errors input#user_email', 1,
+                  'email field should be highlighted'
+
+    post users_path, user: @test_info.merge(email: 'i am not an email address')
+    assert_response 422, 'should error on invalid email'
+    assert_select '.field_with_errors input#user_email', 1,
+                  'email field should be highlighted'
+
+    post users_path, user: @test_info.merge(email: @kylo.email)
+    assert_response 422, 'should error on already-taken password'
+    assert_select '.field_with_errors input#user_email', 1,
+                  'email field should be highlighted'
+  end
+
+  test 'should error on post with invalid password or confirmation' do
+    post users_path, user: @test_info.merge(password: '',
+                                            password_confirmation: '')
+    assert_response 422, 'should error on missing password and confirmation'
+    assert_select '.field_with_errors input#user_password', 1,
+                  'should highlight password field'
+
+    post users_path, user: @test_info.merge(password: 'short',
+                                            password_confirmation: 'short')
+    assert_response 422, 'should error on too-short password'
+    assert_select '.field_with_errors input#user_password', 1,
+                  'should highlight password field'
+
+    post users_path, user:
+                     @test_info.merge(password: 'longbutwrong',
+                                      password_confirmation: 'wrongandlong')
+    assert_response 422, 'should error on mismatched password and confirmation'
+    assert_select '.field_with_errors input#user_password_confirmation', 1,
+                  'should highlight password confirmation field'
   end
 end
