0b189b840278eada08d147ad976106c59501a50a
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-26 11:56:21 -0500

    Add tests for user edit
    Tests for blank form, invalid email, mismatched new passwords, and
    changed login credentials

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,98 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @kylo = users(:kylo)
+  end
+
+  test 'edit with blank fields is rejected' do
+    get edit_user_path(@kylo)
+    assert_template 'users/edit'
+    patch user_path(@kylo), user: { name: '',
+                                    email: '',
+                                    password: '',
+                                    password_confirmation: '' }
+
+    assert_template 'users/edit'
+
+    assert_select 'li', 'Name can\'t be blank',
+                  'Error messages should call out name'
+    assert_select '.field_with_errors input#user_name', 1,
+                  'Name field should be highlighted'
+
+    assert_select 'li', 'Email can\'t be blank',
+                  'Error messages should call out email'
+    assert_select '.field_with_errors input#user_email', 1,
+                  'Email field should be highlighted'
+
+    assert_select 'li', 'Password is too short (minimum is 8 characters)',
+                  'Error messages should call out password'
+    assert_select '.field_with_errors input#user_password', 1,
+                  'Password field should be highlighted'
+  end
+
+  test 'edit with invalid email is rejected' do
+    get edit_user_path(@kylo)
+    assert_template 'users/edit'
+    patch user_path(@kylo), user: { name: @kylo.name,
+                                    email: 'i am not an email address',
+                                    password: 'password',
+                                    password_confirmation: 'password' }
+
+    assert_template 'users/edit'
+
+    assert_select 'li', 'Email is invalid',
+                  'Error messages should call out email'
+    assert_select '.field_with_errors input#user_email', 1,
+                  'Email field should be highlighted'
+  end
+
+  test 'edit with mismatched passwords is rejected' do
+    get edit_user_path(@kylo)
+    assert_template 'users/edit'
+    patch user_path(@kylo), user: { name: @kylo.name,
+                                    email: @kylo.email,
+                                    password: 'password',
+                                    password_confirmation: 'hunter22' }
+
+    assert_template 'users/edit'
+
+    assert_select 'li', 'Password confirmation doesn\'t match Password',
+                  'Error messages should call out password'
+    assert_select '.field_with_errors input#user_password_confirmation', 1,
+                  'Password confirmation field should be highlighted'
+  end
+
+  test 'edit can change login credentials' do
+    new_email = 'i_am_so_powerful@livejournal.com'
+    new_password = 'f34rm3l0zr'
+
+    get edit_user_path(@kylo)
+    assert_template 'users/edit'
+    patch user_path(@kylo), user: { name: @kylo.name,
+                                    email: new_email,
+                                    password: new_password,
+                                    password_confirmation: new_password }
+    assert_redirected_to @kylo, 'should be redirected to the profile page'
+
+    delete logout_path
+
+    post login_path, session: { email: @kylo.email, password: 'password' }
+    assert_template 'sessions/new', 'should be redirected to login page'
+    assert_select '.alert-danger', 'Invalid email/password combination',
+                  'old credentials should be rejected'
+
+    post login_path, session: { email: new_email, password: 'password' }
+    assert_template 'sessions/new', 'should be redirected to login page'
+    assert_select '.alert-danger', 'Invalid email/password combination',
+                  'old password should be rejected'
+
+    post login_path, session: { email: @kylo.email, password: new_password }
+    assert_template 'sessions/new', 'should be redirected to login page'
+    assert_select '.alert-danger', 'Invalid email/password combination',
+                  'old email should be rejected'
+
+    post login_path, session: { email: new_email, password: new_password }
+    assert_redirected_to @kylo, 'should be redirected to the profile page'
+  end
 end
