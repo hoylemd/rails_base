8c95c5c58d568d4d1c4de6c275499c38428a8df2
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 21:44:39 -0500

    Add some element selectors on the login page test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,7 +4,11 @@ class SessionsControllerTest < ActionController::TestCase
   test 'should get new' do
     get :new
     assert_response :success
-    # TODO: assert some things on the page
+    assert_select 'input#session_email', 1, 'one email input should be present'
+    assert_select 'input#session_password', 1,
+                  'one password input should be present'
+    assert_select 'input#session_remember_me', 1,
+                  'one remember input should be present'
   end
 
   # TODO: 'post to create, valid'
