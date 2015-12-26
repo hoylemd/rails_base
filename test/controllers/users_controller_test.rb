3ad8aa027331799a4c7d30a419fa2d2d9d73251e
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 23:35:45 -0500

    move fixture data into setup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test_user = { name: 'Max Payne',
-                email: 'Max_Payne@example.com',
-                password: 'password',
-                password_confirmation: 'password' }
+  def setup
+    @test_info = { name: 'Max Payne',
+                   email: 'Max_Payne@example.com',
+                   password: 'password',
+                   password_confirmation: 'password' }
+    @kylo = users(:kylo)
+  end
 
   test 'should get new' do
     get :new
@@ -21,49 +24,51 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test 'should error on post with missing name' do
-    post :create, user: test_user.merge(name: '')
+    post :create, user: @test_info.merge(name: '')
     assert_response 422, 'should error on missing name'
     assert_select '.field_with_errors input#user_name', 1,
                   'name field should be highlighted'
   end
 
   test 'should error on post with invalid email' do
-    post :create, user: test_user.merge(email: '')
+    post :create, user: @test_info.merge(email: '')
     assert_response 422, 'should error on missing email'
     assert_select '.field_with_errors input#user_email', 1,
                   'email field should be highlighted'
 
-    post :create, user: test_user.merge(email: 'i am not an email address')
+    post :create, user: @test_info.merge(email: 'i am not an email address')
     assert_response 422, 'should error on invalid email'
     assert_select '.field_with_errors input#user_email', 1,
                   'email field should be highlighted'
 
-    post :create, user: test_user.merge(email: 'vader_fan667@hotmail.com')
+    post :create, user: @test_info.merge(email: 'vader_fan667@hotmail.com')
     assert_response 422, 'should error on already-taken password'
     assert_select '.field_with_errors input#user_email', 1,
                   'email field should be highlighted'
   end
 
   test 'should error on post with invalid password or confirmation' do
-    post :create, user: test_user.merge(password: '', password_confirmation: '')
+    post :create, user: @test_info.merge(password: '',
+                                         password_confirmation: '')
     assert_response 422, 'should error on missing password and confirmation'
     assert_select '.field_with_errors input#user_password', 1,
                   'should highlight password field'
 
-    post :create, user: test_user.merge(password: 'short',
-                                        password_confirmation: 'short')
+    post :create, user: @test_info.merge(password: 'short',
+                                         password_confirmation: 'short')
     assert_response 422, 'should error on too-short password'
     assert_select '.field_with_errors input#user_password', 1,
                   'should highlight password field'
 
-    post :create, user: test_user.merge(password: 'longbutwrong',
-                                        password_confirmation: 'wrongandlong')
+    post :create, user: @test_info.merge(password: 'longbutwrong',
+                                         password_confirmation: 'wrongandlong')
     assert_response 422, 'should error on mismatched password and confirmation'
     assert_select '.field_with_errors input#user_password_confirmation', 1,
                   'should highlight password confirmation field'
   end
 
-  # TODO: 'should get show, logged in'
-  # TODO: 'should not get show, not logged in'
+  test 'should get show' do
+  end
+
   # TODO: 'user_params strips out extra parameters'
 end
