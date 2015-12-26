32741aab3acfd21163481f6dc2bca06bbc613a79
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 23:13:24 -0500

    Add test for invalid signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,14 +7,41 @@ class UsersControllerTest < ActionController::TestCase
     assert_select 'title', 'Sign Up | Ruby on Rails Tutorial Sample App'
   end
 
-  test 'should post create, valid' do
+  test 'should redirect on post to create with valid information' do
     post :create, user: { name: 'George Carlin',
                           email: 'george_carlin@example.com',
                           password: 'password',
                           password_confirmation: 'password' }
     assert_response :redirect, 'should redirect to user profile'
   end
-  # TODO: 'should post create, invalid'
+
+  test 'should post create, invalid' do
+    test_user = { name: 'Max Payne',
+                  email: 'Max_Payne@example.com',
+                  password: 'password',
+                  password_confirmation: 'password' }
+
+    post :create, user: test_user.merge(name: '')
+    assert_response 422, 'should error on missing name'
+
+    post :create, user: test_user.merge(email: '')
+    assert_response 422, 'should error on missing email'
+
+    post :create, user: test_user.merge(password: '', password_confirmation: '')
+    assert_response 422, 'should error on missing password and confirmation'
+
+    post :create, user: test_user.merge(password: 'longbutwrong',
+                                        password_confirmation: 'wrongandlong')
+    assert_response 422, 'should error on mismatched password and confirmation'
+
+    post :create, user: test_user.merge(password: 'short',
+                                        password_confirmation: 'short')
+    assert_response 422, 'should error on too-short password'
+
+    post :create, user: test_user.merge(email: 'vader_fan667@hotmail.com')
+    assert_response 422, 'should error on too-short password'
+  end
+
   # TODO: 'should get show, logged in'
   # TODO: 'should not get show, not logged in'
   # TODO: 'user_params strips out extra parameters'
