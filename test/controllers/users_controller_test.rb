ee7a304b24e634fcabe1e2109c557b41e6c9aa32
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 22:42:59 -0500

    Add a test for valid signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,7 +7,13 @@ class UsersControllerTest < ActionController::TestCase
     assert_select 'title', 'Sign Up | Ruby on Rails Tutorial Sample App'
   end
 
-  # TODO: 'should post create, valid'
+  test 'should post create, valid' do
+    post :create, user: { name: 'George Carlin',
+                          email: 'george_carlin@example.com',
+                          password: 'password',
+                          password_confirmation: 'password' }
+    assert_response :redirect, 'should redirect to user profile'
+  end
   # TODO: 'should post create, invalid'
   # TODO: 'should get show, logged in'
   # TODO: 'should not get show, not logged in'
