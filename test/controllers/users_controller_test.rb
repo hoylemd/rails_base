543632d614c95893aada1e7ce8b23b047444745f
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-25 23:26:28 -0500

    start breaking the invalid tests out
    They should be their own cases

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  test_user = { name: 'Max Payne',
+                email: 'Max_Payne@example.com',
+                password: 'password',
+                password_confirmation: 'password' }
+
   test 'should get new' do
     get :new
     assert_response :success
@@ -15,15 +20,14 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :redirect, 'should redirect to user profile'
   end
 
-  test 'should post create, invalid' do
-    test_user = { name: 'Max Payne',
-                  email: 'Max_Payne@example.com',
-                  password: 'password',
-                  password_confirmation: 'password' }
-
+  test 'should error on post with missing name' do
     post :create, user: test_user.merge(name: '')
     assert_response 422, 'should error on missing name'
+    assert_select '.field_with_errors input#user_name', 1,
+                  'name field should be highlighted'
+  end
 
+  test 'should post create, invalid' do
     post :create, user: test_user.merge(email: '')
     assert_response 422, 'should error on missing email'
 
@@ -39,7 +43,7 @@ class UsersControllerTest < ActionController::TestCase
     assert_response 422, 'should error on too-short password'
 
     post :create, user: test_user.merge(email: 'vader_fan667@hotmail.com')
-    assert_response 422, 'should error on too-short password'
+    assert_response 422, 'should error on already-taken password'
   end
 
   # TODO: 'should get show, logged in'
