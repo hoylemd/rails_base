fd9162e027943bdf83126cf81c99fec7e4bfb2d6
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-27 10:06:41 -0500

    Add 100 seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Adding Leia Organa'
+User.create!(name: 'Leia Organa',
+             email: 'leia@rebelalliance.org',
+             password: 'password',
+             password_confirmation: 'password')
+
+99.times do |n|
+  name = Faker::Name.name
+  puts "Adding #{name}"
+  email = "example-#{n + 1}@railstutorial.org"
+  password = 'password'
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
