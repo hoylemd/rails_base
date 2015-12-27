06e2d75b2412550cad1a4ab225ee9cbacebaea59
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-26 22:39:39 -0500

    Add unauthenticated tests for user edit and update
    Need to be able to detect security regressions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,28 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @kylo = users(:kylo)
+  end
+
   test 'should get new' do
     get :new
     assert_response :success
     assert_select 'title', 'Sign Up | Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'should redirect edit when not logged in' do
+    get :edit, id: @kylo
+
+    assert_flash type: :danger, expected: 'Please log in.'
+    assert_redirected_to login_url, 'Should redirect to login page'
+  end
+
+  test 'should redirect update when not logged in' do
+    patch :update, id: @kylo, user: { name: @kylo.name, email: @kylo.email }
+
+    assert_flash type: :danger, expected: 'Please log in.'
+    assert_redirected_to login_url, 'Should redirect to login page'
   end
 
   # I can't figure out how to go the GET here. Keeps saying:
