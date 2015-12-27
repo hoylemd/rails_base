9f6256e862a327285f6e35b16210a0dacd4b1b8f
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-27 11:14:00 -0500

    Add a test for the users index page

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @kylo = users(:kylo)
+  end
+
+  test 'index has pagination and at least 30 users, with links to profiles' do
+    log_in_as @kylo
+    get users_path
+    assert_select 'div.pagination', 2, 'Should see 2 pagination controls'
+    assert_select '.users li', 30, 'Should see 30 users'
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
 end
