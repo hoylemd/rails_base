3ec08f70c0cc952a05d7cbc62c59b34755555c17
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-28 12:44:28 -0500

    Add activation token hooks
    Needs to create the token and digest before creation, so the activation
    email can be sent right away.
    
    Also moved the before save hook into a private method, to keep things
    consistent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)*([a-z]+)\z/i
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save do
-    email.downcase!
-  end
+  before_create :create_activation
+  before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -42,5 +41,18 @@ class User < ActiveRecord::Base
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  # Creates the activation token
+  def create_activation
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  # normalize emails
+  def downcase_email
+    email.downcase!
   end
 end
