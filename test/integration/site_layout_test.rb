0d7fac0395585daa1331b3a0562cf6b6cb38fb11
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-27 20:13:37 -0500

    More layout tests
    Make sue the layout links don't change

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,16 +1,40 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layout links' do
+  def setup
+    @kylo = users(:kylo)
+  end
+
+  test 'layout links, unauthenticated' do
     get root_path
 
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', users_path, count: 0
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', signup_path
 
     get signup_path
     assert_select 'title', full_title('Sign Up')
+  end
+
+  test 'layout links, authenticated' do
+    log_in_as @kylo
+    get root_path
+
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', users_path, count: 1
+    assert_select 'a[href=?]', user_path(@kylo), count: 1
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path, count: 1
+    assert_select 'a[href=?]', signup_path, count: 1
   end
 end
