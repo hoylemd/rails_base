303e39df742fd3d014344af6b46bc3401a36d42f
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-29 15:01:12 -0500

    Update signup tests to reflect new process
    Shouldn't log in, should go to the correct page, and should look for the
    correct flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,14 +9,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     @kylo = users(:kylo)
   end
 
-  def assert_signup_succeeded(name = nil)
-    assert_template 'users/show', 'Should be on profile page'
+  def assert_signup_succeeded
+    assert_template 'static_pages/home', 'Should be on home page'
 
     assert_no_error_messages
 
-    assert_flash type: 'success', expected: name ? "Welcome, #{name}!" : nil
+    assert_flash type: 'info',
+                 expected: 'Please check your email to activate your account.'
 
-    assert logged_in?, 'User should be logged in'
+    assert_not logged_in?, 'User should not be logged in'
   end
 
   def assert_signup_failed(errors)
@@ -45,7 +46,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             remember_me: '1' }
     end
 
-    assert_signup_succeeded name
+    assert_signup_succeeded
   end
 
   test 'post to create should ignore extra parameters' do
@@ -59,7 +60,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             id: 'so lonely :(' }
     end
 
-    assert_signup_succeeded name
+    assert_signup_succeeded
     # This tests user_params - the id field will be stripped out, and not cause
     # an error.  If it were not stripped out, an error would be raised because
     # ids are integers - not strings.
@@ -77,7 +78,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             admin: true }
     end
 
-    assert_signup_succeeded name
+    assert_signup_succeeded
     assert_not User.find_by(email: email).admin, 'Should not be an admin'
   end
 
