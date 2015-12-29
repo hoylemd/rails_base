9a09437474170e8094bad346c2a410730933f4ec
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-29 14:21:39 -0500

    Add tests for account activation

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,20 +1,23 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test "account_activation" do
-    mail = UserMailer.account_activation
-    assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+  def setup
+    @kylo = users(:kylo)
   end
 
-  test "password_reset" do
+  test 'account_activation' do
+    mail = UserMailer.account_activation @kylo
+    assert_equal 'Account activation', mail.subject
+    assert_equal [@kylo.email], mail.to
+    assert_equal ['noreply@example.com'], mail.from
+    assert_match "Hi #{@kylo.name},", mail.body.encoded
+  end
+
+  test 'password_reset' do
     mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal 'Password reset', mail.subject
+    assert_equal ['to@example.org'], mail.to
+    assert_equal ['noreply@example.com'], mail.from
+    assert_match 'Hi', mail.body.encoded
   end
-
 end
