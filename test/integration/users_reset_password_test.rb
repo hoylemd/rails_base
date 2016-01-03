0f218dc61280acaf0d91f8b74f4599de06767c9c
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-03 16:18:00 -0500

    Stub out some test for the password reset

---
+++ test/integration/users_reset_password_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersResetPasswordTest < ActionDispatch::IntegrationTest
+  test 'main reset flow' do
+  end
+
+  test 'unknown email behaves the same as known email' do
+  end
+end
