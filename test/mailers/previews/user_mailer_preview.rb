b7c49e837dbd38f84e6f9fd37f6e25334bb58760
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-03 17:18:10 -0500

    Update the mail previews
    So I can use them.

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,18 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/email_verification
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/email_verification
   def email_verification
     user = User.first
     user.verification_token = User.new_token
     UserMailer.email_verification user
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
