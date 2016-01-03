17605e36a261b0b80db786928131ae6e5402dbab
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-03 15:15:37 -0500

    Add skeleton of password reset flow
    Just pretends that all emails submitted don't exist.
    
    The feedback for password will be identical betwen successful and failed
    reset requests, so bad guys can't fish for accounts.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,38 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(email: reset_email)
+
+    if user
+      # if found, create reset digest and timestamp
+      # and send email
+    end
+
+    # redirect to root with (potentially misinformative) flash message
+    show_password_reset_feedback user
+    redirect_to root_path
+  end
+
   def edit
+  end
+
+  private
+
+  def show_password_reset_feedback(user)
+    flash[:info] = 'A password reset link has been emailed to you'
+
+    return unless Rails.env.development?
+    if user
+      user.reload
+      reset_link = '/password_resets/new'
+      flash[:info] += ". dev: <a href=\"#{reset_link}\">reset_link</a>"
+    else
+      flash[:info] += ". dev: No account for #{reset_email} found."
+    end
+  end
+
+  def reset_email
+    @reset_email ||= params[:password_reset][:email].downcase
   end
 end
