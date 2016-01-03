2864e415d6ab37ac54a9475255a1280ff0fd1dab
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-02 22:54:25 -0500

    Update the signup test to check email verification
    Security feature = must test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     ActionMailer::Base.deliveries.clear
   end
 
+  def get_verify_emails_for(email)
+    ActionMailer::Base.deliveries.select do |item|
+      (item.to.include?(email) &&
+       item.subject == 'Email verification')
+    end
+  end
+
+  def get_verify_token_from_email(message)
+    token_regex = /[a-zA-Z0-9\-_]{22}/
+
+    text_body = message.text_part.body.raw_source
+
+    token_regex.match(text_body)[0]
+  end
+
   def assert_signup_succeeded(spec = nil)
     assert_template 'static_pages/home', 'Should be on home page'
 
@@ -19,6 +34,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                  expected: 'Please check your email to verify your email.'
 
     assert_not logged_in?, 'User should not be logged in'
+
+    return unless spec && spec[:email]
+    messages = get_verify_emails_for spec[:email]
+    assert_equal 1, messages.length,
+                 'Should see exactly 1 Email verification sent to ' \
+                   "#{spec[:email]}, saw #{messages.length}"
   end
 
   def assert_signup_failed(errors)
@@ -47,7 +68,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: spec
     end
 
-    assert_signup_succeeded
+    assert_signup_succeeded spec
+    assert_not logged_in?, 'User should not be logged in yet'
+
+    verify_emails = get_verify_emails_for spec[:email]
+    verify_token = get_verify_token_from_email verify_emails[0]
+    assert verify_token, 'Email should contain a valid token'
+
+    user = User.find_by(email: spec[:email])
+    assert_not user.verified?, 'User\'s email should not be verified yet'
+
+    get edit_email_verification_path(verify_token, email: user.email)
+
+    assert user.reload.verified?, 'User\'s email should be verified'
+    follow_redirect!
+    assert_template 'users/show', 'Should be on the user profile page'
+    assert logged_in?, 'User should be logged in'
   end
 
   test 'post to create should ignore extra parameters' do
