e8bd6d123e9a2de0e9092b627e30b197fadcac07
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-02 19:57:35 -0500

    Implement verification action
    yay it works! now to test it...

--- app/controllers/email_verifications_controller.rb
+++ app/controllers/email_verifications_controller.rb
@@ -1,4 +1,20 @@
-class AccountActivationsController < ApplicationController
+class EmailVerificationsController < ApplicationController
+  def verify_email(user)
+    user.update_attribute(:verified, true)
+    user.update_attribute(:verified_at, Time.zone.now)
+    log_in user
+    flash[:success] = 'Email verified!'
+    redirect_to user
+  end
+
   def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.verified? &&
+       user.authenticated?(:verification, params[:id])
+      verify_email user
+    else
+      flash[:danger] = 'Invalid verification link'
+      redirect_to root_url
+    end
   end
 end
