9679124bcdcfb8638c2fea3ec7aad1d12af6262a
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-07 15:34:51 -0500

    Add a general page layout test for user profile
    This will let me be more confident that I won't mess things up.

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,10 +1,30 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
   def setup
     @kylo = users(:kylo)
     @peaches = users(:peaches)
     @batman = users(:batman)
+  end
+
+  test 'profile display' do
+    get user_path(@peaches)
+    assert_template 'users/show', 'Should be on the user profile page'
+    assert_select 'title', full_title(@peaches.name),
+                  'Title should display user\'s name'
+    assert_select 'h1', { text: @peaches.name },
+                  'User\'s name should appear in a header'
+    assert_select 'h1>img.gravatar', true,
+                  'User\'s gravatar should appear in a header'
+    assert_match @peaches.microposts.count.to_s, response.body,
+                 'Count of user\'s microposts should appear in body'
+    assert_select 'div.pagination', true, 'Pagination controls should appear'
+    @peaches.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body,
+                   'Micropost content should be correct'
+    end
   end
 
   test 'User profile page should show at most 30 microposts' do
