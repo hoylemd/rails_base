0caabbed614c4d2e084cebd685e8e63a721fa2b6
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-07 15:37:26 -0500

    Add routes and controller for microposts
    Start of allowing micropost manipulation.

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
 
   resources :email_verifications, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
