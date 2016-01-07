8a88cb77a79fc455c5cfb3136f96f7036876624b
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-07 14:40:48 -0500

    Add db seeds for microposts
    So I have some data to devlop with

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107003803) do
+ActiveRecord::Schema.define(version: 20160107001751) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,10 @@ User.create!(name: 'Leia Organa',
                verified: true,
                verified_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+puts "Adding posts for #{users.map(&:name).join ', '}"
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
