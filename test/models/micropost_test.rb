fce4b94bfe5c5fd6257b5acc043a67601a67bf6b
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-07 00:51:44 -0500

    Add tests and validations for microposts

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @kylo = users(:kylo)
+    # This code is not idiomatically correct.
+    @micropost = Micropost.new(
+      user_id: @kylo.id,
+      content: 'i think dad\'s walking carpet resents me for some reason')
+  end
+
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user id should be present' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
