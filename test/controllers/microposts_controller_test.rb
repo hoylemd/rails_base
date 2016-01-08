5cf359c49c72fa21a44a30449b57597fad36ad31
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-08 15:51:21 -0500

    Add a test + stubs for micropost delete

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -12,10 +12,29 @@ class MicropostsControllerTest < ActionController::TestCase
     assert_template 'sessions/new', 'Should be redirected to login page'
   end
 
+  test 'should accept create when logged in' do
+  end
+
   test 'should redirect destroy when not logged in' do
     assert_no_difference 'Micropost.count' do
       delete :destroy, id: @parsecs
     end
     assert_template 'sessions/new', 'Should be redirected to login page'
+  end
+
+  test 'should redirect destroy when not owning tweet' do
+    log_in_as users(:batman)
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: @parsecs
+    end
+    assert_flash type: :danger,
+                 expected: 'Sorry, you don\'t have permission to do that'
+    assert_redirected_to root_path, 'Should be redirected to home page'
+  end
+
+  test 'should accept destroy when correct user' do
+  end
+
+  test 'should accept destroy when admin' do
   end
 end
