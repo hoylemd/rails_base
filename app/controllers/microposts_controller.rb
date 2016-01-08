19ab7fe054c859ec9311c92c7315821282a00e42
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-08 16:00:56 -0500

    Implement destroy action ACL
    Don't want the wrong people deleting things!

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user, only: [:destroy]
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -19,5 +20,13 @@ class MicropostsController < ApplicationController
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+
+    return if @micropost
+    flash[:danger] = 'Sorry, you don\'t have permission to do that'
+    redirect_to root_path
   end
 end
