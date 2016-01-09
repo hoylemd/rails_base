6b6d8d966e306cc5b6e5ead1f4386328414870c6
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-09 14:45:46 -0500

    Refactor the session controller tests a bit
    make them nicer, and I also added a stub for a happy-path test for
    session/new

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -15,6 +15,13 @@ class SessionsControllerTest < ActionController::TestCase
                   'one remember input should be present'
   end
 
+  # TODO: implement this
+  test 'post to create should create session on correct credentials' do
+    # no credentials
+    # wrong un/password
+    # correct credentials
+  end
+
   test 'should redirect to profile on post valid credentials to create' do
     post :create, session: { email: @kylo.email, password: 'password' }
     assert_redirected_to @kylo, 'should be redirected '
@@ -22,15 +29,19 @@ class SessionsControllerTest < ActionController::TestCase
 
   test 'should error on post invalid credentials to create' do
     post :create, session: { email: @kylo.email, password: 'letmeinplease' }
-    assert_response 401, 'should report a 401 UNAUTHORIZED error'
+    assert_response :unauthorized, 'should report a 401 UNAUTHORIZED error'
   end
 
-  test 'delete to destroy' do
+  test 'delete to destroy clears session, if logged in' do
+    delete :destroy
+    assert_redirected_to login_path,
+                         'Should redirect to login page when not logged in'
+
     log_in_as @kylo
-    assert_equal @kylo.id, session[:user_id], 'should be logged in'
 
     delete :destroy
-    assert_redirected_to login_url, 'should redirect to login page'
-    assert session[:user_id].nil?, 'user id should be removed from session'
+    assert_redirected_to login_url,
+                         'Should redirect to login page when logged in'
+    assert session[:user_id].nil?, 'User id should be removed from session'
   end
 end
