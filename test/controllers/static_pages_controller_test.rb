22498337126ff1ac5cf9d20cb8a94fc58a9ce376
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-09 14:48:08 -0500

    Add some assert messages to static pages tests
    These should also check that they layout have what they should but that
    can happen later.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,27 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # TODO: fill these out with layout assertions
   test 'should get home' do
     get :home
-    assert_response :success
+    assert_response :success, 'Should receive a 200 OK on GET to home'
     assert_select 'title', 'Ruby on Rails Tutorial Sample App'
   end
 
   test 'should get help' do
     get :help
-    assert_response :success
+    assert_response :success, 'Should receive a 200 OK on GET to help'
     assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
   end
 
   test 'should get about' do
     get :about
-    assert_response :success
+    assert_response :success, 'Should receive a 200 OK on GET to help'
     assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
   end
 
   test 'should get contact' do
     get :contact
-    assert_response :success
+    assert_response :success, 'Should receive a 200 OK on GET to contact'
     assert_select 'title', 'Contact | Ruby on Rails Tutorial Sample App'
   end
 end
