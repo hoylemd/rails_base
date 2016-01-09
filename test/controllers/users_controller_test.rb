05d256daaf71981668865c8f2c0bc621a5dd68c8
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-09 14:49:32 -0500

    Add an assert message to user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,7 +9,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test 'should get new' do
     get :new
-    assert_response :success
+    assert_response :success, 'Should receive 200 OK on GET to new'
     assert_select 'title', 'Sign Up | Ruby on Rails Tutorial Sample App'
   end
 
