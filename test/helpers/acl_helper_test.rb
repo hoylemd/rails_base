03c1a870f1e045c71bf23d7b96d5d25f34dd66f4
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-09 19:14:17 -0500

    Fill out acl helper tests
    tests both helper methods now!

--- test/helpers/acl_helper_test.rb
+++ test/helpers/acl_helper_test.rb
@@ -5,28 +5,54 @@ class AclHelperTest < ActionView::TestCase
 
   def setup
     @kylo = users(:kylo)
+    @batman = users(:batman)
     @peaches = users(:peaches)
   end
 
-  test 'current_user? tests the current user' do
+  test 'current_user? fails for no user' do
     assert_not current_user?(@kylo), 'Not logged in should be false'
-    log_in_as @peaches
+  end
+
+  test 'current_user? fails for wrong user' do
+    log_in_as @batman
     assert_not current_user?(@kylo), 'Wrong user should be false'
-    assert current_user?(@peaches), 'Correct user should be true'
+  end
+
+  test 'current_user? fails for wrong user, even if they are admin' do
+    log_in_as @peaches
+    assert_not current_user?(@kylo), 'Wrong (admin) user should be false'
+  end
+
+  test 'current_user? passes for the current user' do
+    log_in_as @kylo
+    assert current_user?(@kylo), 'Correct user should be true'
   end
 
   test 'correct_user? passes for correct current user' do
+    log_in_as @kylo
+
+    assert correct_user? user: @kylo
   end
 
   test 'correct_user? passes for admin current user' do
+    log_in_as @peaches
+
+    assert correct_user? user: @kylo
   end
 
   test 'correct_user? fails for no user' do
+    assert_not correct_user? user: @kylo
   end
 
   test 'correct_user? fails for wrong user' do
+    log_in_as @batman
+    assert_not correct_user? user: @kylo
   end
 
   test 'correct_user? uses passed test' do
+    test = proc { |user| user == @kylo }
+
+    assert correct_user? user: @kylo, test: test
+    assert_not correct_user? user: @peaches, test: test
   end
 end
