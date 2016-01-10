57841168f318eff5924cc6200a37a884d631d024
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-10 13:17:42 -0500

    Add server-side validations to pictures.
    5mb limit, and only images.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,12 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+  def picture_size
+    return unless picture.size > 5.megabytes
+    errors.add(:picture, 'must be 5 megabytes or less.')
+  end
 end
