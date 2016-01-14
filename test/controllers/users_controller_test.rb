e0c89f4d0251baf6893806a81fe5a1eb91c6b81a
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-14 15:00:29 -0500

    finally fixed that user show test!
    I realised I need to use symbols to trigger the methods. of course.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -89,12 +89,9 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to users_path, 'Should be redirected to user index page'
   end
 
-  # I can't figure out how to go the GET here. Keeps saying:
-  # ActionController::UrlGenerationError Exception: No route matches
-  #   {:action=>"/users/33989797", :controller=>"users"}
-  # test 'should get show' do
-  #   get user_path(id: @kylo.id)
-  #   assert_response :success, 'should return 200 OK'
-  #   assert_select 'title', 'Kylo Ren | Ruby on Rails Tutorial Sample App'
-  # end
+  test 'should get show' do
+    get :show, id: @kylo.id
+    assert_response :success, 'should return 200 OK'
+    assert_select 'title', 'Kylo Ren | Ruby on Rails Tutorial Sample App'
+  end
 end
