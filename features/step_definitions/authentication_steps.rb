dd77483a130414d614885dec58da8fa9d3fd3fe9
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-19 15:47:25 -0500

    Add steps for entering username and password
    Another step towards testing the login flow.  I might be breaking this
    stuff up too much, but it's very DRY and modular!

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -18,29 +18,39 @@ def unverified_identity
   {
     name: 'Barack Obama',
     email: 'potus@not.us.gov',
-    password: 'hunter2'
+    password: 'password'
   }
 end
 
-def log_in(credentials)
-  @current_email = credentials[:email]
-  @current_password = credentials[:password]
+def identity
+  @identity ||= user_identity
+end
 
+def enter_email(email = identity[:email])
+  @current_email = email
+  fill_in 'Email', with: @current_email
+end
+
+def enter_password(password = identity[:password])
+  @current_password = password
+  fill_in 'Password', with: @current_password
+end
+
+def log_in(credentials = identity)
   visit_page('login')
 
-  fill_in 'Email', with: @current_email
-  fill_in 'Password', with: @current_password
+  enter_email credentials[:email]
+  enter_password credentials[:password]
 
   click_button 'Log in'
 end
 
 Given(/I am logged into the app$/) do
-  @identity ||= user_identity
-  log_in @identity
+  log_in identity
 end
 
-Given(/I am a( regular user|n admin|n unverified user)$/) do |identity|
-  case identity
+Given(/I am a( regular user|n admin|n unverified user)$/) do |who|
+  case who
   when ' regular user'
     @identity = user_identity
   when 'n admin'
@@ -48,4 +58,12 @@ Given(/I am a( regular user|n admin|n unverified user)$/) do |identity|
   when 'n unverified user'
     @identity = unverified_identity
   end
+end
+
+When(/I enter my email$/) do
+  enter_email
+end
+
+When(/I enter my password$/) do
+  enter_password
 end
