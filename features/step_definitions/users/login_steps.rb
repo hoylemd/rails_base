f3d04ab0f5cf819d63df3073c6276ac0617d4b3f
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-20 13:39:30 -0500

    merge the login state check steps

--- features/step_definitions/users/login_steps.rb
+++ features/step_definitions/users/login_steps.rb
@@ -3,22 +3,17 @@ Given(/I am logged in$/) do
 end
 
 def logged_in?(negate = false)
+  # TODO: this should actually check cookies
   if negate
-    # TODO: implement this (checking cookies)
     assert_see_links('users', 0)
     assert_see_links('signup', 2)
     assert_see_links('login', 1)
   else
-    # TODO: this should actually check cookies
     assert_see_links('users', 1)
     assert page.html.include? 'Log out'
   end
 end
 
-Then(/I should be logged in$/) do
-  logged_in?
-end
-
-Then(/I should not be logged in$/) do
-  logged_in? true
+Then(/I should( not)? be logged in$/) do |negate|
+  logged_in? negate
 end
