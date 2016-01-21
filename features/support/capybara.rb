cc25b2761152bb7f0e49913b24985568667c8197
Michael D. Hoyle <hoylemd@gmail.com>
2016-01-21 15:24:40 -0500

    Embed screenshots in reports
    Seems like a good idea, even though I'm not really using reports just
    yet

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -11,6 +11,6 @@ After do |scenario|
   if scenario.failed?
     path = "screenshots/debug_#{Time.now.to_i}.png"
     page.save_screenshot(path)
-    print "Screenshot saved to #{path}\n"
+    embed path, 'image/png', 'SCREENSHOT'
   end
 end
